23e73a95dae19a7ff95da33da19dca578fce1434
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-10-24 17:15:56 -0700

    10/24/21 commit

--- ninja.js
+++ ninja.js
@@ -22,3 +22,29 @@ console.log(ninja1.sayName());
 console.log(ninja1.showStats());
 console.log(ninja1.drinkSake());
 console.log(ninja1.showStats());
+
+class Sensei extends Ninja {
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        const drink = super.drinkSake();
+        let num = Math.floor(Math.random() * 3);
+        if(num === 0){
+            return "Eat bitter, taste sweet. " + drink;
+        }
+        if(num === 1){
+            return "Fall down seven, get up eight. " + drink;
+        }
+        if(num === 2){
+            return "A journey of a thousand miles begins with a single step. " + drink;
+        }
+    }
+}
+
+const sensei1 = new Sensei("Christian");
+console.log(sensei1.speakWisdom());
